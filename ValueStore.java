b2ad9ea3e26fb9de60418e8719aa991b5914e823
Owen Medina <owensmedi@gmail.com>
2017-11-22 15:21:19 +0800

    finished writing to data.values (Brian, Owen, Matt)

---
+++ ValueStore.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.lang.*;
+
+public class ValueStore{
+	public static final int INIT_RECORDS = 0;
+	public static final int INIT_POS = 0;
+	public static long numRec;
+	public static RandomAccessFile raf;
+
+	//constructor
+	public ValueStore(String strFile) throws IOException{
+		File file = new File(strFile);
+		if(!file.exists()){
+			//make raf and file
+			raf = new RandomAccessFile(strFile, "rwd");
+			file.createNewFile();
+
+			//write 0 as # of records
+			raf.writeLong(INIT_RECORDS);
+			numRec = INIT_RECORDS;
+		}else{
+			raf = new RandomAccessFile(file, "rwd");
+
+			raf.seek(INIT_POS);
+			//read first long, which is # of records
+			numRec = raf.readLong();
+		}
+	}
+
+	public void addValue(String val) throws IOException{
+		raf.seek(8 + numRec*256);
+		raf.writeBytes(val);
+		raf.seek(INIT_RECORDS);
+		raf.writeLong(++numRec);
+	}
+	
+}
--- btdb.java
+++ btdb.java
@@ -1,7 +1,29 @@
 import java.util.*;
+import java.io.*;
 
-public class btdb{
-	public static void main(String[] args){
-		System.out.println("TESTGITLUL");
+public class btdb {
+	public static final int FIRST_ARG = 0;
+	public static final int SECOND_ARG = 1;
+	public static final int THIRD_ARG = 2;
+	public static void main(String[] args)  throws IOException {
+		Scanner in = new Scanner(System.in); //process cmds
+		String btName = args[FIRST_ARG];
+		String valuesName = args[SECOND_ARG];
+
+		//checks if two file names have been typed
+		if(checkArgs(args)){
+			ValueStore vs = new ValueStore(valuesName);
+			while(in.hasNext()){
+				String[] cmd = in.nextLine().split(" ");
+				if(cmd[FIRST_ARG].equals("insert")){
+
+				}
+				vs.addValue(cmd[THIRD_ARG]);
+			}
+		}
+	}
+
+	public static boolean checkArgs(String[] args){
+		return (args.length == 2);
 	}
 }
