469710f8f45b3f69aa439ef895f4dc4874332743
Owen Medina <owensmedi@gmail.com>
2017-11-27 21:04:46 +0800

    added minor sorting method to B Tree addValue

--- BTree.java
+++ BTree.java
@@ -4,7 +4,7 @@ import java.lang.*;
 public class BTree{
 	public static final long INIT_POS = 0;
 	public static final long INIT_VAL = 0;
-	public static final long DEF_VALUE = -1;
+	public static final int DEF_VALUE = -1;
 	public static final int ARR_LENGTH = 20;
 	public static final int ORDER = 7;
 	public static final int BYTES_PER_ENTRY = 8;
@@ -33,17 +33,30 @@ public class BTree{
 
 	public void initRec() throws IOException {
 		for(int i = 0; i < BYTES_PER_NODE; i++){
-			raf.writeLong(DEF_VALUE);
+			raf.writeByte(DEF_VALUE);
 		}
 	}
 
 	public void addValue(long key, long vNumRec) throws IOException{
+		//check if number of nodes is still 0
+		//change number of nodes to 1 if yes (first value to be inserted into file)
 		raf.seek(INIT_POS);
 		if(raf.readLong()==0){
 			raf.seek(INIT_POS);
 			raf.writeLong(1);
 		}
-		raf.seek(HEADER_BYTES+btNumRec*BYTES_PER_NODE);
+
+		//find correct index for the new value
+		long correctInd = btNumRec; //default place to put new value is at the end of the list
+		for(long ind = 0; ind < btNumRec; ind++){
+			raf.seek(HEADER_BYTES+ind*BYTES_PER_NODE);
+			long currKey = raf.readLong();
+			if(currKey == DEF_VALUE || currKey > key){
+				correctInd = ind;
+				break;
+			}
+		}
+		raf.seek(HEADER_BYTES+correctInd*BYTES_PER_NODE);
 		raf.writeLong(key);
 		raf.writeLong(vNumRec);
 		btNumRec++;
