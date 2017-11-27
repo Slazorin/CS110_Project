fd6e5a7dd051f4e9646ba2e94745cb8971aa6c7f
Brian Pacia <brianjeffreyppacia@yahoo.com>
2017-11-27 20:54:16 +0800

    Fixed when value has spaces or is empty

--- btdb.java
+++ btdb.java
@@ -5,6 +5,7 @@ public class btdb {
 	public static final int FIRST_ARG = 0;
 	public static final int SECOND_ARG = 1;
 	public static final int THIRD_ARG = 2;
+	public static final int NUM_COMMANDS = 3;
 	public static void main(String[] args)  throws IOException {
 		Scanner in = new Scanner(System.in); //process cmds
 		String btName = args[FIRST_ARG];
@@ -15,7 +16,10 @@ public class btdb {
 			ValueStore vs = new ValueStore(valuesName);
 			BTree bt = new BTree(btName);
 			while(in.hasNext()){
-				String[] cmd = in.nextLine().split(" ");
+				String[] cmd = new String[NUM_COMMANDS];
+				cmd[FIRST_ARG] = in.next();
+				cmd[SECOND_ARG] = Integer.toString(in.nextInt());
+				cmd[THIRD_ARG] = in.nextLine().replaceFirst(" ","");
 				if(cmd[FIRST_ARG].equals("insert")){
 					long numRec = vs.addValue(cmd[THIRD_ARG]);
 					bt.addValue(Long.parseLong(cmd[SECOND_ARG]), numRec);
