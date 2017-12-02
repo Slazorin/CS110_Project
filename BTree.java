f472a95afdc70f239a313d591b1e2398ef08a51e
Brian Pacia <brianjeffreyppacia@yahoo.com>
2017-12-02 13:18:50 +0800

    Got simpleInsert to work plus some node methods (Brian,Owen)

--- BTree.java
+++ BTree.java
@@ -3,17 +3,21 @@ import java.lang.*;
 import java.util.*;
 
 public class BTree{
+	public static long btNumRec = 0;
 	public static final long INIT_POS = 0;
 	public static final long INIT_VAL = 0;
 	public static final long DEF_VALUE = -1;
-	public static final long ARR_LENGTH = 20;
+	public static final long REC_LENGTH = 20;
+	public static final long IND_FIRST_CHILD = 1;
 	public static final int ORDER = 7;
 	public static final long BYTES_PER_ENTRY = 8;
+	public static final long NOT_PARENT = -1;
 	public static final long BYTES_PER_NODE = ((3*ORDER)-1)*BYTES_PER_ENTRY;
 	public static final int HEADER_BYTES = 16;
 	public static final int LAST_OFFSET = 18;
 	public static final int NUM_POINTERS = 3*ORDER-1;
-	public static long btNumRec = 0;
+	public static final long MAX_KEYS = ORDER - 1;
+	public static final long END_OF_FILE = HEADER_BYTES + BYTES_PER_NODE*btNumRec; 
 	public static ValueStore vs;
 	public static RandomAccessFile raf;
 
@@ -29,14 +33,16 @@ public class BTree{
 			//when creating, putting first value as well
 			raf.writeLong(INIT_VAL); //number of records
 			raf.writeLong(INIT_VAL); //position of root node
-			initRec();
+			initRec(INIT_POS);
 		}else{
 			raf = new RandomAccessFile(file, "rwd");
 		}
 		vs = valueS;
 	}
 
-	public void initRec() throws IOException {
+	public void initRec(long index) throws IOException {
+		//Fills up a node with -1s
+		raf.seek(index);
 		for(int i = 0; i < NUM_POINTERS; i++){
 			raf.seek(BYTES_PER_ENTRY*i+HEADER_BYTES);
 			raf.writeLong(DEF_VALUE);
@@ -59,11 +65,141 @@ public class BTree{
 			return verdict;
 		}
 
+		//if key is valid find its correct place
+		//find place on bottom most-level (default place to insert without splitting)
+		long initCorrRec = 0;
+		boolean indFound = false;
+		long availInd = MAX_KEYS; //3*btNumRec+2; //first available index
+		long corrInd = availInd;// first correct is last of node
+
+		//finds correct node/bt record number
+		raf.seek(HEADER_BYTES+(initCorrRec*BYTES_PER_ENTRY*REC_LENGTH)+BYTES_PER_ENTRY);
+		long child1ID = raf.readLong();
+		while(initCorrRec <= btNumRec || child1ID == NOT_PARENT ){
+			//finds correct index
+			for(long ind = 2; ind < availInd*3; ind+=3){
+				raf.seek(HEADER_BYTES+(initCorrRec*BYTES_PER_ENTRY*REC_LENGTH)+ind*BYTES_PER_ENTRY);
+				long currKey = raf.readLong();
+				if(currKey == DEF_VALUE || currKey > key){
+					corrInd = ind;
+					indFound = true;
+					System.out.println(corrInd + " " + currKey); //tester
+					break;
+
+				}
+			}
+			if(indFound){
+				break;
+			}
+			System.out.println("HI");//tester
+			initCorrRec++;
+			if(initCorrRec > btNumRec){
+				break;
+			}
+			System.out.println("FAIL");//tester
+			raf.seek(HEADER_BYTES+(initCorrRec*BYTES_PER_ENTRY*REC_LENGTH)+IND_FIRST_CHILD);
+			child1ID = raf.readLong();
+
+		}
+
+		if(!isFull(initCorrRec)){
+			//btNumRec++;
+			return simpleInsert(key, vNumRec, initCorrRec, corrInd);
+		}else{
+			//insert all keys in corrNode into key array
+			long[] keys = new long[ORDER-1];
+			int arrInd = 0;
+			for(int ind = 2; ind < REC_LENGTH; ind+=3){
+				raf.seek(HEADER_BYTES+(initCorrRec*BYTES_PER_ENTRY*REC_LENGTH)+ind);
+				keys[arrInd++] = raf.readLong(); 
+			}
+			keys[arrInd-1] = key;
+
+			long median = getMedian(keys, key);
+			long promoted = key;
+			if(key != median)
+				promoted = median;
+			System.out.println("LOL"); //test
+			//long finCorrRec = findFree(initCorrRec);
+			//promote(promoted, )
+		}
+
+		return verdict;
+
 	}
+
+	public String simpleInsert(long key, long vNumRec, long initCorrRec, long corrInd) throws IOException{
+		String verdict = "";
+		shiftValues(initCorrRec, corrInd);
+		raf.seek(HEADER_BYTES+(initCorrRec*BYTES_PER_ENTRY*REC_LENGTH)+corrInd*BYTES_PER_ENTRY);
+		raf.writeLong(key);
+		raf.writeLong(vNumRec);
+		verdict = key + " inserted.";
+		return verdict;
+
+	}
+
+	public long childCount(long node) throws IOException{
+		long count = 0;
+		long ind = HEADER_BYTES + node*BYTES_PER_NODE + BYTES_PER_ENTRY;
+		boolean isChild = true;
+		while(isChild){
+			long curr = raf.readLong();
+			if( curr != DEF_VALUE ) {
+				count++;
+			}else {
+				isChild = false;
+			}
+			ind += BYTES_PER_ENTRY*2;
+		}
+		return count;
+	}
+
+	public long keyCount(long node) throws IOException{
+		long count = 0;
+		long ind = HEADER_BYTES + node*BYTES_PER_NODE + BYTES_PER_ENTRY*2;
+		boolean isKey = true;
+		while(isKey){
+			raf.seek(ind);
+			long curr = raf.readLong();
+			if( curr != DEF_VALUE ) {
+				count++;
+			}else {
+				isKey = false;
+			}
+			ind += BYTES_PER_ENTRY*3;
+		}
+		return count;
+	}
+	public void makeParentNode(long key, long vNumRec) throws IOException{
+		//Goes to EOF and creates node full of -1s
+		initRec(END_OF_FILE);
+		raf.seek(HEADER_BYTES + btNumRec*BYTES_PER_NODE + BYTES_PER_ENTRY);
+		raf.writeLong(key);
+		raf.writeLong(vNumRec);
+	}
+
+	public void split(long promotedKey, long initCorrRec) throws IOException{
+		makeParentNode(promotedKey,0);
+	}
+
+	public long getParent(long node) throws IOException{
+		//Grabs the parent node record num
+		raf.seek(HEADER_BYTES + node*REC_LENGTH*BYTES_PER_ENTRY);
+		long parentID = raf.readLong();
+		return parentID;
+	}
+
+	public void setParent(long node, long newParent) throws IOException {
+		//Sets the parent of a node
+		raf.seek(HEADER_BYTES + node*REC_LENGTH*BYTES_PER_ENTRY);
+		raf.writeLong(newParent);
+	}
+
 
 	public void shiftRecord(long copyFromIndex, long copyToIndex) throws IOException{
 		//Reads each long data from current record and writes to the target one
-		for( long i = 0; i < ARR_LENGTH*BYTES_PER_ENTRY; i += BYTES_PER_ENTRY ){
+		for( long i = 0; i < REC_LENGTH*BYTES_PER_ENTRY; i += BYTES_PER_ENTRY ){
 			raf.seek(copyFromIndex);
 			long curr = raf.readLong();
 			raf.seek(copyToIndex);
@@ -71,14 +207,16 @@ public class BTree{
 		}
 	}
 
-	public void shiftValues(long correctInd) throws IOException{
-		for(long ind = 3*btNumRec+2; ind > correctInd; ind-=3){
+	public void shiftValues(long node, long correctInd) throws IOException{
+		long keyCount = keyCount(node);
+		System.out.println(keyCount);
+		for(long ind = 3*keyCount+2; ind > correctInd; ind-=3){
 			//read value from previous position
-			raf.seek(HEADER_BYTES+(ind-3)*BYTES_PER_ENTRY);
+			raf.seek(HEADER_BYTES + (node*BYTES_PER_NODE) + (ind-3)*BYTES_PER_ENTRY);
 			long prevKey = raf.readLong();
 			long prevVNumRec = raf.readLong();
 			//put values into next position
-			raf.seek(HEADER_BYTES+ind*BYTES_PER_ENTRY);
+			raf.seek(HEADER_BYTES+ (node*BYTES_PER_NODE) + ind*BYTES_PER_ENTRY);
 			raf.writeLong(prevKey);
 			raf.writeLong(prevVNumRec);
 
@@ -98,10 +236,10 @@ public class BTree{
 		return isCopy;
 	}
 
-	public boolean isFull(long node){
-		raf.seek(node*BYTES_PER_ENTRY*ARR_LENGTH + BYTES_PER_ENTRY*LAST_OFFSET);
+	public boolean isFull(long node) throws IOException{
+		raf.seek(node*BYTES_PER_ENTRY*REC_LENGTH + HEADER_BYTES + LAST_OFFSET*BYTES_PER_ENTRY);
 		long curr = raf.readLong();
-		if( curr == DEF_VALUE ){
+		if( curr != DEF_VALUE ){
 			return true;
 		}
 		return false;
