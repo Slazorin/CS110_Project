5a3b134764ba649426423bb35a0fed568346cd81
Owen Medina <owensmedi@gmail.com>
2017-12-06 23:17:51 +0800

    Brian implemented a printTree methods that prints an easy to read tree

--- btdb.java
+++ btdb.java
@@ -6,6 +6,7 @@ public class btdb {
 	public static final int SECOND_ARG = 1;
 	public static final int THIRD_ARG = 2;
 	public static final int NUM_COMMANDS = 3;
+	public static final long DEF_VALUE = -1;
 	public static void main(String[] args)  throws IOException {
 		Scanner in = new Scanner(System.in); //process cmds
 		String btName = args[FIRST_ARG];
@@ -19,13 +20,14 @@ public class btdb {
 				String[] cmd = new String[NUM_COMMANDS];
 				cmd[FIRST_ARG] = in.next();
 				if(cmd[FIRST_ARG].equals("exit")){
+					bt.printTree();
 					System.exit(0);
 				}
 				cmd[SECOND_ARG] = Integer.toString(in.nextInt());
 				cmd[THIRD_ARG] = in.nextLine().replaceFirst(" ","");
 				if(cmd[FIRST_ARG].equals("insert")){
 					long numRec = vs.addValue(cmd[THIRD_ARG]);
-					String verdict = bt.addValue(Long.parseLong(cmd[SECOND_ARG]), numRec);
+					String verdict = bt.addValue(Long.parseLong(cmd[SECOND_ARG]), numRec, DEF_VALUE);
 					System.out.println(verdict);
 				}else if(cmd[FIRST_ARG].equals("select")){
 					String verdict = bt.select(Long.parseLong(cmd[SECOND_ARG]));
